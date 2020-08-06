fdf5e24234df26eab484bf2d9aadda222429ee82
Ron McLaren <ronlaren@aol.com>
2020-08-06 11:00:48 -0400

    Fixedd small bug were a string was wrong.

--- src/Ingester/BufrParser/BufrDescription.cpp
+++ src/Ingester/BufrParser/BufrDescription.cpp
@@ -14,20 +14,18 @@ using namespace eckit;
 
 const string BUFR_PARSER_YAML_SECTION = "bufrParser";
 const string MNEMONIC_SETS_YAML_SECTION = "mnemonicSets";
-const string MNEMONIC_SET_YAML_NAME = "name";
+const string MNEMONIC_STR_YAML_NAME = "mnemonics";
 const string PER_ELEMENT_SIZE_YAML_NAME = "perElementSize";
 
 BufrDescription::BufrDescription(const string& filepath)
 {
-    auto yaml = YAMLConfiguration(PathName(filepath));
-
-    auto conf = yaml.getSubConfiguration(BUFR_PARSER_YAML_SECTION)
-                    .getSubConfiguration(MNEMONIC_SETS_YAML_SECTION);
+    auto conf = YAMLConfiguration(PathName(filepath)).getSubConfiguration(BUFR_PARSER_YAML_SECTION)
+                                                     .getSubConfiguration(MNEMONIC_SETS_YAML_SECTION);
 
 
     for (const auto& mnemonicSetConf : conf.getSubConfigurations())
     {
-        addMnemonicSet(MnemonicSet(mnemonicSetConf.getString(MNEMONIC_SET_YAML_NAME),
+        addMnemonicSet(MnemonicSet(mnemonicSetConf.getString(MNEMONIC_STR_YAML_NAME),
                                    mnemonicSetConf.getInt(PER_ELEMENT_SIZE_YAML_NAME)));
 
     }
